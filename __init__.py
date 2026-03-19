8211dab0d89a77b6b44bc162c14618495a0a5076
Michael James Quidilla <mjamezquidilla@gmail.com>
2026-03-19 23:23:14 +0800

    chore: modified __init__

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from Truss_2D.Truss_2D import Truss_2D
-from Truss_3D.Truss_3D import Truss_3D
-from Frame_2D import Frame_2D
-import matplotlib.pyplot as plt
-plt.style.use('fivethirtyeight')
-plt.rcParams['hatch.color'] = 'white'
+# from struqlab_engine.Frame_2D import Frame_2D, Member_2D
+# from struqlab_engine.Frame_2D.Frame_2D_builders import Frame_builder
+# from struqlab_engine.Truss_2D.Truss_2D import Truss_2D
+# from struqlab_engine.Truss_3D.Truss_3D import Truss_3D
+
+from .Frame_2D import Frame_2D, Frame_2D_builders
+from .Truss_2D import Truss_2D
+from .Truss_3D import Truss_3D
 
 # print("Positive Values for forces: right, up (righthand rule)")
 # print("Negative moment = clockwise, Positive moment = counter-clockwise (righthand rule)")
